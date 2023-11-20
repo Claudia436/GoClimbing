bf9c08733beec8e1118dd3f71b7c5fdb3cd84d26
Claudia436 <clasan436@alu.centroafuera.es>
2023-11-20 19:29:26 +0100

    commit3

--- app/src/main/java/com/example/goclimbing/ListActivity.kt
+++ app/src/main/java/com/example/goclimbing/ListActivity.kt
@@ -3,35 +3,68 @@ package com.example.goclimbing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.toObject
 
 class ListActivity : AppCompatActivity() {
-    private lateinit var database: DatabaseReference
+    lateinit var lista: RecyclerView
+    val adapter = RocodromoAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
-        database = Firebase.database.reference
+        setUpList()
 
-        recibirLista()
+        //database = Firebase.database
+        val database = Firebase.firestore
+
+        val docRef = database.collection("Rocodromos")
+        docRef.get()
+            .addOnSuccessListener { documents ->
+                if (documents != null) {
+                    var rocodromos = ArrayList<Rocodromo>()
+
+                    for (document in documents) {
+                        val rocodromo = document.toObject<Rocodromo>()
+                        Log.d("rocodromo", "${rocodromo}")
+                        rocodromos.add(rocodromo)
+                    }
+
+                    adapter.submitList(rocodromos)
+
+                    Log.d("document", "DocumentSnapshot data: ${documents}")
+                    // convertir document en nuestra class Rocodromos
+                    // dicha clase es la que vamos a pasar a nuestra lista
+                } else {
+                    Log.d("document", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("", "get failed with ", exception)
+            }
+
     }
 
-    fun recibirLista() {
-        database.child("Rocodromos").get().addOnSuccessListener {
-            Toast.makeText(baseContext, "Got value ${it.value}", Toast.LENGTH_LONG).show()
-            Log.i("firebase rocodromos", "Got value ${it.value}")
-
-
-        }.addOnFailureListener{
-            Toast.makeText(baseContext, "error getting value", Toast.LENGTH_LONG).show()
-            Log.e("firebase rocodromos", "Error getting data", it)
-        }
-
+    private fun setUpList() {
+        lista = findViewById(R.id.list)
+        lista.adapter = adapter
     }
-
-
 }
+
+data class Rocodromo(
+    val Id: String? = null,
+    val Nombre: String? = null,
+    val Descripcion: String? = null,
+    val Horario: String? = null,
+    val Latitud: String? = null,
+    val Longitud: String? = null,
+    val Imagen: String? = null,
+    val Instalaciones: String? = null,
+    val Tarifas: String? = null,
+    val Web: String? = null,
+    val Ubicacion: String? = null
+)
---
+++ app/src/main/java/com/example/goclimbing/RocodromoAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.goclimbing
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class RocodromoAdapter : ListAdapter<Rocodromo, RocodromoAdapter.RocodromoViewHolder>(RocodromoDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RocodromoViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.rocodromo_item, parent, false)
+        return RocodromoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RocodromoViewHolder, position: Int) {
+        val rocodromo = getItem(position)
+        holder.bind(rocodromo)
+    }
+
+    class RocodromoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val nombre: TextView = view.findViewById(R.id.nombre)
+
+        fun bind(rocodromo: Rocodromo) {
+            nombre.text = rocodromo.Nombre
+        }
+    }
+
+}
+
+class RocodromoDiffCallback : DiffUtil.ItemCallback<Rocodromo>() {
+    override fun areItemsTheSame(oldItem: Rocodromo, newItem: Rocodromo): Boolean {
+        return oldItem.Id == newItem.Id
+    }
+    override fun areContentsTheSame(oldItem: Rocodromo, newItem: Rocodromo): Boolean {
+        return oldItem == newItem
+    }
+}
